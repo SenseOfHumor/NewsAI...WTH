5b1db845beac20807b1683a9f8369fc296852ba2
SWPNL <sd2269@njit.edu>
2024-06-16 02:09:55 +0530

    added a card functionality to make the news appear as cards, the title will be the news title and the text will be the sub heading? dunno will find out while actually building the AI app, will functionalize everything

---
+++ cardTest.py
@@ -0,0 +1,38 @@
+import streamlit as st
+import sys
+import time
+
+import base64
+
+# To use a local image, but i believe, if a json file is used, the image can be stored in the json file.. dunno, will find out
+with open("flat-design-illustration-wallpaper-preview.jpg", "rb") as f:
+    data = f.read()
+    encoded = base64.b64encode(data)
+data = "data:image/png;base64," + encoded.decode("utf-8")
+
+
+def card1():  ## inspired by https://github.com/gamcoh/st-card
+    from streamlit_card import card
+
+    card = card(
+    title="",
+    text="pip install love",
+    image=data,
+    #url="https://github.com/gamcoh/st-card"
+
+
+    styles={
+        "card": {
+            "width": "100%", # <- make the card use the width of its container, note that it will not resize the height of the card automatically
+            "height": "300px" # <- if you want to set the card height to 300px
+            
+        }
+    }
+
+    )
+
+
+
+if st.button("show card"):  # function call to test the functionality of the card
+    card1()
+

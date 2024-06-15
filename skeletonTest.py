07c0fcb69ab443807afd5f250496dcff4182a0f6
SWPNL <sd2269@njit.edu>
2024-06-16 01:47:24 +0530

    turned the skeleton thingy into a callable function

--- skeletonTest.py
+++ skeletonTest.py
@@ -1,17 +1,19 @@
 import streamlit as st
 import time
 
-st.title('Skeleton Test')
+def skeleton():
+    with st.spinner("Showing skeleton..."):
+        placeholder = st.empty()  # create a placeholder
+
+        # show the skeleton in the placeholder
+        from streamlit.proto.Skeleton_pb2 import Skeleton as SkeletonProto
+        placeholder._enqueue("skeleton",  SkeletonProto())
+
+        time.sleep(2)  # wait for 2 seconds
+
+        placeholder.empty() 
+        st.write("Cleared the skeleton") # clear the placeholder
 
 
-if st.button('Click to see skeleton'):
-    placeholder = st.empty()  # create a placeholder
-
-    # show the skeleton in the placeholder
-    from streamlit.proto.Skeleton_pb2 import Skeleton as SkeletonProto
-    placeholder._enqueue("skeleton",  SkeletonProto())
-
-    time.sleep(2)  # wait for 2 seconds
-
-    placeholder.empty() 
-    st.write("Cleared the skeleton") # clear the placeholder
+if st.button("show skeleton"):
+        skeleton()
